72274cec87bc8b71bef52e64c20b8441d6591c4c
manonbk <manon.ben-kiran@polytechnique.edu>
2024-07-24 14:41:36 +0200

    Mise à jour : questions ouvertes et fermées possibles

--- questionnaire.py
+++ questionnaire.py
@@ -4,20 +4,33 @@ import time
 
 st.write("Questionnaire")
 
+# Lire les questions et les options à partir du fichier
 with open("questions.txt") as f:
     questions = f.readlines()
 
+# Charger les réponses précédentes
 df = pd.read_csv("awnsers.csv", index_col=0)
 awns = {}
 
 day = time.strftime(r"%d/%m/%Y")
 
+# Afficher les questions à choix multiple et les questions ouvertes
 for q in questions:
-    awns[q] = st.text_input(q, key=q)
+    q_type, question = q.strip().split('|', 1)
+    if q_type == 'multiple':
+        question, options = question.split('|')
+        options = options.split(',')
+        awns[question] = st.selectbox(question, options, key=question)
+    elif q_type == 'open':
+        awns[question] = st.text_input(question, key=question)
 
+# Ajouter les nouvelles réponses au dataframe
 new = pd.DataFrame(awns, index=[day])
 df = pd.concat([df, new], ignore_index=False)
 
+# Afficher le dataframe mis à jour
 st.write(df)
+
+# Enregistrer les réponses lorsqu'on clique sur le bouton
 if st.button("Save"):
     df.to_csv("awnsers.csv")
