ef46b9ee6fbeba5a9b671b1cab00d247e1cc5f58
manonbk <manon.ben-kiran@polytechnique.edu>
2024-07-24 14:53:12 +0200

    Updqte : first question everyday, other 3 at random

--- questionnaire.py
+++ questionnaire.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import time
+import random
 
 st.write("Questionnaire")
 
@@ -8,14 +9,30 @@ st.write("Questionnaire")
 with open("questions.txt") as f:
     questions = f.readlines()
 
+# Séparer la première question des autres
+first_question = questions[0]
+remaining_questions = questions[1:]
+
 # Charger les réponses précédentes
 df = pd.read_csv("awnsers.csv", index_col=0)
 awns = {}
 
 day = time.strftime(r"%d/%m/%Y")
 
-# Afficher les questions à choix multiple et les questions ouvertes
-for q in questions:
+# Ajouter la première question
+q_type, question = first_question.strip().split('|', 1)
+if q_type == 'multiple':
+    question, options = question.split('|')
+    options = options.split(',')
+    awns[question] = st.selectbox(question, options, key=question)
+elif q_type == 'open':
+    awns[question] = st.text_input(question, key=question)
+
+# Sélectionner 3 questions aléatoires parmi les restantes
+random_questions = random.sample(remaining_questions, 3)
+
+# Afficher les questions aléatoires
+for q in random_questions:
     q_type, question = q.strip().split('|', 1)
     if q_type == 'multiple':
         question, options = question.split('|')
